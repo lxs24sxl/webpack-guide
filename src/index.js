3fa04bd5f5cbf2ec49eb32035f442b7320f1623f
lxs24sxl <13539748633@139.com>
2018-05-30 11:59:32 +0800

    修改目录结构,将lodash植入本地

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 function component() {
   let element = document.createElement('div');
 
-  // loadsh(目前通过一个script脚本引入)对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], '' );
+  // lodash, now imported by this script
+  element.innerHTML = _.join(['Hello ', 'webpack'], '' );
 
   return element;
 };
