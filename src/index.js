1ac889549c633debd58b5584f13ea13abc85d8e7
lxs24sxl <13539748633@139.com>
2018-05-31 17:06:44 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+// import _ from 'lodash';
+// import printMe from './print.js';
+// import './styles.css';
+import { cube } from './main.js';
 
 function component() {
-  let element = document.createElement('div');
-  let btn = document.createElement( 'button' );
+  // let element = document.createElement('div');
+  let element = document.createElement( 'pre' );  
 
   // lodash, now imported by this script
-  element.innerHTML = _.join(['Hello ', 'webpack4.10.1'], '' );
+  // element.innerHTML = _.join(['Hello ', 'webpack4.10.1'], '' );
 
-  btn.innerHTML = 'Click me and check the console！';
-  btn.onclick = printMe;
-
-  element.appendChild( btn );
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube( 5 )
+  ].join('\n\n');
 
   return element;
 };
@@ -21,13 +22,13 @@ function component() {
 // document.body.appendChild(element);
 document.body.appendChild(component());
 
-console.log(module.hot);
-if ( module.hot ) {
-  module.hot.accept('./print.js', function () {
-    console.log( 'Accepting the updated printMe module!' );
-    printMe();
-  //   document.body.removeChild( element );
-  //   element = component();    // 重新渲染页面后，component 更新 click 事件处理
-  //   document.body.appendChild( element );
-  });
-}
+// console.log(module.hot);
+// if ( module.hot ) {
+//   module.hot.accept('./print.js', function () {
+//     console.log( 'Accepting the updated printMe module!' );
+//     printMe();
+//   //   document.body.removeChild( element );
+//   //   element = component();    // 重新渲染页面后，component 更新 click 事件处理
+//   //   document.body.appendChild( element );
+//   });
+// }
