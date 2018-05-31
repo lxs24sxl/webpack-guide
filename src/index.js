bbb362a8c447f647223840e9e75f9d10c08b70ef
lxs24sxl <13539748633@139.com>
2018-05-31 14:37:08 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css';
 
 function component() {
   let element = document.createElement('div');
@@ -16,4 +17,17 @@ function component() {
   return element;
 };
 
+// let element = component();
+// document.body.appendChild(element);
 document.body.appendChild(component());
+
+console.log(module.hot);
+if ( module.hot ) {
+  module.hot.accept('./print.js', function () {
+    console.log( 'Accepting the updated printMe module!' );
+    printMe();
+  //   document.body.removeChild( element );
+  //   element = component();    // 重新渲染页面后，component 更新 click 事件处理
+  //   document.body.appendChild( element );
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const webpack = require('webpack');
 
 module.exports = {
   // entry: './src/index.js',
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
   },
   devtool:'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
+  },
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
+      title: 'Hot Module Replacement'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve( __dirname, 'dist'),
-    publicPath: '/'
+    // publicPath: '/'
   },
 };
